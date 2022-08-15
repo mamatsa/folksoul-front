4ed8fc9ab9d813cca14372907f0d8663556ec024
otomamatsashvili <otar.mamatsashvili@gmail.com>
2022-08-15 11:32:29 +0400

    feat: integrate react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Login } from 'pages';
 
 const App = () => {
   return (
-    <>
-      <Login />
-    </>
+    <Router>
+      <Routes>
+        <Route path='/login' element={<Login />} />
+        <Route path='/dashboard' element={<h1>დილამშვიდობისა!</h1>} />
+      </Routes>
+    </Router>
   );
 };
 
--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -2,6 +2,7 @@ import { Input, Button, LoginWrapper } from 'pages/Login/components';
 import { useForm, SubmitHandler } from 'react-hook-form';
 import { loginRequest } from 'services';
 import { AxiosError } from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 type Inputs = {
   nickname: string;
@@ -16,20 +17,24 @@ const Login = () => {
     formState: { errors },
   } = useForm<Inputs>();
 
+  const navigate = useNavigate();
+
   const onSubmit: SubmitHandler<Inputs> = async (data) => {
     try {
       const response = await loginRequest(data.nickname, data.password);
-      console.log(response);
+      if (response.status === 'success') {
+        navigate('/dashboard');
+      }
     } catch (error) {
       const err = error as AxiosError;
       const status = err.response?.status;
       if (status === 400) {
         setError('nickname', {
-          type: 'custom',
+          type: 'backend',
           message: 'არასწორი მომხმარებელი ან პაროლი',
         });
         setError('password', {
-          type: 'custom',
+          type: 'backend',
           message: 'არასწორი მომხმარებელი ან პაროლი',
         });
       }
