24d5c899ffd84e6ca5805548ac9bb1867386a807
otomamatsashvili <otar.mamatsashvili@gmail.com>
2022-08-20 10:24:51 +0400

    chore: add band members related svgs and colors

---
+++ src/components/svgs/ViewButton.tsx
@@ -0,0 +1,14 @@
+const ViewButton = () => (
+  <svg
+    width='24'
+    height='24'
+    viewBox='0 0 24 24'
+    fill='none'
+    xmlns='http://www.w3.org/2000/svg'
+  >
+    <circle cx='12' cy='12' r='11.5' fill='black' stroke='#88D06F' />
+    <circle cx='12' cy='12' r='6' fill='#88D06F' />
+  </svg>
+);
+
+export default ViewButton;
