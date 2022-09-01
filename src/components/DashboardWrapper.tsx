254839cbcc577fa36784126996b256f50dd2dfab
otomamatsashvili <otar.mamatsashvili@gmail.com>
2022-09-01 10:28:47 +0400

    fix: change band info edit button position

--- src/components/DashboardWrapper.tsx
+++ src/components/DashboardWrapper.tsx
@@ -35,7 +35,7 @@ const DashboardWrapper: React.FC<{ children: React.ReactNode }> = (props) => {
         </ul>
       </nav>
       <div className='w-full h-[90vh] flex justify-center items-center'>
-        <div className='relative h-full w-11/12 flex flex-col justify-evenly items-center bg-dashboard-bg shadow-inside rounded-[20px] md:w-4/5'>
+        <div className='h-full w-11/12 flex flex-col justify-evenly items-center bg-dashboard-bg shadow-inside rounded-[20px] md:w-4/5'>
           {props.children}
         </div>
       </div>
--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -45,43 +45,45 @@ const About = () => {
 
   return (
     <DashboardWrapper>
-      <DashboardPageTitle title='ბენდის შესახებ' />
-      <div className='w-[93%] h-full flex flex-col items-center gap-12  py-8 mb-20 overflow-y-scroll overflow-x-hidden scrollbar-thin scrollbar-track-scrollbar-blue scrollbar-thumb-primary-dark-blue scrollbar-thumb-rounded scrollbar-track-rounded'>
-        <div className='relative w-52 h-52 min-h-52 mx-5 p-[7px] rounded-full bg-about-purple flex justify-center items-center drop-shadow-thin'>
-          <div className='w-full h-full rounded-full flex items-center justify-center overflow-hidden'>
-            {bandInformation?.imageUrl && (
-              <img
-                src={
-                  process.env.REACT_APP_BASE_URL! + bandInformation?.imageUrl
-                }
-                alt='ავატარი'
-                className='w-full h-auto'
-              />
-            )}
-          </div>
+      <div className='relative w-full h-full flex flex-col justify-evenly items-center'>
+        <DashboardPageTitle title='ბენდის შესახებ' />
+        <div className='w-[93%] h-full flex flex-col items-center gap-12  py-8 mb-20 overflow-y-scroll overflow-x-hidden scrollbar-thin scrollbar-track-scrollbar-blue scrollbar-thumb-primary-dark-blue scrollbar-thumb-rounded scrollbar-track-rounded'>
+          <div className='relative w-52 h-52 min-h-52 mx-5 p-[7px] rounded-full bg-about-purple flex justify-center items-center drop-shadow-thin'>
+            <div className='w-full h-full rounded-full flex items-center justify-center overflow-hidden'>
+              {bandInformation?.imageUrl && (
+                <img
+                  src={
+                    process.env.REACT_APP_BASE_URL! + bandInformation?.imageUrl
+                  }
+                  alt='ავატარი'
+                  className='w-full h-auto'
+                />
+              )}
+            </div>
 
-          <Link
-            to='#'
-            className='absolute bottom-1 right-0 w-14 h-14'
-            onClick={() => {
-              setShowImageUploadModal(true);
-            }}
-          >
-            <EditPhotoButton />
+            <Link
+              to='#'
+              className='absolute bottom-1 right-0 w-14 h-14'
+              onClick={() => {
+                setShowImageUploadModal(true);
+              }}
+            >
+              <EditPhotoButton />
+            </Link>
+          </div>
+          {bandInformation?.about && (
+            <div className='flex justify-center'>
+              <p className='mx-24 text-justify whitespace-pre-line'>
+                {bandInformation.about}
+              </p>
+            </div>
+          )}
+        </div>
+        <div className='absolute h-2/3 bottom-0 left-10 flex items-center'>
+          <Link to='/about-band/edit'>
+            <EditTextButton />
           </Link>
         </div>
-        {bandInformation?.about && (
-          <div className='flex justify-center'>
-            <p className='mx-24 text-justify whitespace-pre-line'>
-              {bandInformation.about}
-            </p>
-          </div>
-        )}
-      </div>
-      <div className='absolute h-2/3 bottom-0 left-10 flex items-center'>
-        <Link to='/about-band/edit'>
-          <EditTextButton />
-        </Link>
       </div>
       {showImageUploadModal && (
         <ImageUploadModal
