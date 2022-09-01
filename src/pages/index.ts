0bcc9c6d1e5aaf80cb540a529db3d71f0e020aa7
otomamatsashvili <otar.mamatsashvili@gmail.com>
2022-09-01 11:22:39 +0400

    chore: create empty landing component

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -3,3 +3,4 @@ export { default as Dashboard } from './Dashboard';
 export { default as BandMembers } from './BandMembers';
 export { default as SocialLinks } from './SocialLinks';
 export { default as About } from './About';
+export { default as Landing } from './Landing';
